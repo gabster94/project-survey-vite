c0e00040c91af1e41fb1ece440e04b578b22abbf
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-16 21:59:02 +0200

    - worked on App

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Header } from "./components/Header.jsx"
 import { MultiStepForm } from "./components/SubminNew.jsx";
 
 export const App = () => {
-  return (
-    <div>
-      <Header />
-      <MultiStepForm />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <MultiStepForm />
+        </div>
+    )
 };
