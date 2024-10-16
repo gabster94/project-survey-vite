8bfea8b039b08e811652ab4a808bb1946952959c
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-16 20:32:11 +0200

    - changed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+
 import { Header } from "./components/Header.jsx"
-import { Submit } from "./components/Submit.jsx"
+
 
 
 // import { App } from "./App.jsx";
@@ -9,13 +9,8 @@ export const App = () => {
   return (
     <div>
       <Header />
-      <Submit /> 
-      {/* <form>
-        <Q1 />
-        <Q2 />
-        <Q3 />
-        
-      </form>*/}
+
+
     </div>
   )
 };
