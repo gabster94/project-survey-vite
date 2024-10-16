a9b575252b5dc43257d9178b077645781a84d43c
gabster94 <iofe.gabriella@gmail.com>
2024-10-16 18:42:45 +0200

    Did the Q3

--- src/components/Q3.jsx
+++ src/components/Q3.jsx
@@ -1,24 +1,45 @@
-const Dropdown = () => {
-    const [costume, setCostume] = useState("");
+
+// Jennies suggestion
+// const Dropdown = () => {
+//     const [costume, setCostume] = useState("");
+
+//     return (
+//         <form>
+//             <label>What would your superhero costume be made of?</label>
+//             <select onChange={event => setCostume(event.target.value)}
+//                 value={costume}>
+//                 <option value="">Select costume:</option>
+//                 <option value="">Glitter and spandex</option>
+//                 <option value="">Rubber and feathers</option>
+//                 <option value="">Cardboard and duct tape</option>
+//             </select>
+//         </form>
+//     )
+// }
+
+
+
+// Diegos suggestion
+export const Costume = ({ updateFormData, value }) => {
+    // Function to handle the user's selection and update the form's favoriteBand field
+    const handleFavoriteCostumeChange = (e) => {
+      // Call updateFormData to update the "favoriteBand" field with the selected value
+      updateFormData("favoriteCostume", e.target.value);
+    };
 
     return (
         <form>
-            <select onChange={event => setCostume(event.target.value)}
-                value={costume}
-            >
+            {/* Label for the favorite band dropdown */}
+            <label>What would your superhero costume be made of?</label>
 
-
-
+            {/* Dropdown to select the user's favorite band, controlled by the "value" prop */}
+            {/* The onChange event triggers the handleFavoriteBandChange function to update the form data */}
+            <select value={value} onChange={handleFavoriteCostumeChange}>
+                <option value="">Select costume:</option>
+                <option value="">Glitter and spandex</option>
+                <option value="">Rubber and feathers</option>
+                <option value="">Cardboard and duct tape</option>
             </select>
         </form>
     )
 }
-
-// What would your superhero costume be made of?
-// Glitter and spandex
-// Rubber and feathers
-// Cardboard and duct tape
-
-<Hippopotamus />
-
-<p>superhero superhero</p>
