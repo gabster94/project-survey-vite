2132a7041bc55defd080ff5c39e63858adbf646d
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-16 21:39:48 +0200

    - added radio button question and fixed the SubminNew

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import React from "react";
+
 import { Header } from "./components/Header.jsx"
-// import { Submit } from "./components/Submit.jsx"
-
-
-// // import { App } from "./App.jsx";
+import { MultiStepForm } from "./components/SubminNew.jsx";
 
 export const App = () => {
   return (
     <div>
       <Header />
-      <Submit />
-      {/* <form>
-        <Q1 />
-        <Q2 />
-        <Q3 />
-        
-      </form>*/}
+      <MultiStepForm />
     </div>
   )
 };
--- src/components/Q2.jsx
+++ src/components/Q2.jsx
@@ -1,1 +1,35 @@
 // RadioButtons
+import { useState } from "react";
+
+export const SuperheroStrength = ({ updateFormData, value }) => {
+    const [strength, setStrength] = useState(value)
+    const handleStrengthChange = (event) => {
+        const selectedStrength = event.target.value;
+        setStrength(selectedStrength);
+        updateFormData("strength", selectedStrength); // Update parent state
+    };
+    return (
+        <form>
+            Superhero Strength: What kind of strength would you like to have?
+            <label>
+                <input
+                    type="radio"
+                    value="Superhuman Strength"
+                    onChange={handleStrengthChange}
+                    checked={strength === "Superhuman Strength"}
+                />
+                Superhuman Strength
+            </label>
+            <label>
+                <input
+                    type="radio"
+                    value="Telekinesis"
+                    onChange={handleStrengthChange}
+                    checked={strength === "Telekinesis"}
+                />
+                Telekinesis
+            </label>
+        </form>
+    )
+}
+
